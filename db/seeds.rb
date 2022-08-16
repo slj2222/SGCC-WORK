26854df97be92e4019a0e6107552f5cc51dbcf72
Spencer Johnson <sljohnson2222@gmail.com>
2022-08-15 22:31:15 -0500

    testing

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+member1 = Member.create(first_name: "Spencer", last_name: "Johnson", phone_number: 4172095022, membership_number: 3913, email_address: "sljohnson2225@gmail.com")
+
